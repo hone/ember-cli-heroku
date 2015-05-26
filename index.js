42a1e88db8900e32644886d53ff7778e9e20d0de
Terence Lee <hone02@gmail.com>
2015-05-26 19:38:56 -0400

    make HEROKU_API_PATH avail for build time

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-heroku'
+  name: 'ember-cli-heroku',
+  config: function(env, baseConfig) {
+    return { HEROKU_API_PATH: process.env.HEROKU_API_PATH };
+  }
 };
